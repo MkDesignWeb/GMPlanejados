ae453b46e931005ae5565c22f57de8b2bc1f09ac
MkDesignWeb <99919855+MkDesignWeb@users.noreply.github.com>
2022-08-06 23:16:26 -0300

    Update index.tsx

--- src/Componentes/Modal/index.tsx
+++ src/Componentes/Modal/index.tsx
@@ -6,7 +6,7 @@ import { Container, Cmodal, CImagem, CTexto, Loading, Close, CImagemPlus, ImgPlu
 import { AiOutlineClose, AiOutlineWhatsApp } from 'react-icons/ai'
 import { BiLink } from 'react-icons/bi'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import {Helmet} from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
 
 const GET_MOVEIS_BY_SLUG = gql`
@@ -42,123 +42,125 @@ query GetImgPlusBySlug ($slugImgPlus: String){
 `
 
 interface ModalMoveis {
-    moveisDb: {
-        name: string,
-        slug: string,
-        img: {
-          url: string
-        },
-        description: string,
-        imgPlus: [
-            ImgPlus: {
-            slugImgPlus: string,
-            imgPlus: {
-              url: string
-            }
-        }
-        ]
-      }
-    }
-    
-
-interface GetImgPlusProps {
-    imgPlus: {
+  moveisDb: {
+    name: string,
+    slug: string,
+    img: {
+      url: string
+    },
+    description: string,
+    imgPlus: [
+      ImgPlus: {
+        slugImgPlus: string,
         imgPlus: {
           url: string
-        },
-        slugImgPlus: string
+        }
       }
+    ]
+  }
 }
 
-      
 
-function Modal(){
-  
+interface GetImgPlusProps {
+  imgPlus: {
+    imgPlus: {
+      url: string
+    },
+    slugImgPlus: string
+  }
+}
+
+
+
+function Modal() {
+
   const { slug, slugImgPlus } = useParams()
-  
+
   const { data } = useQuery<ModalMoveis>(GET_MOVEIS_BY_SLUG, {
-        variables: {
-          slug: slug
-        }
-      })
-      
-      const getImgPlus = useQuery<GetImgPlusProps>(GET_MOVEIS_IMG_PLUS_BY_SLUG, {
-        variables: {
-            slugImgPlus: slugImgPlus
-          }
-        })
-        
-        const textUrl = `${window.location.href}\n\n${data?.moveisDb.name}.\n\n De uma olhada nesse móvel feito pela GMPlanejado.`
-
-        
-        const texto = window.encodeURIComponent(textUrl) ;
-
-    if( !data ){
-        return(
-            <Container>
-                <Loading>
-                <Oval
-                        ariaLabel="loading-indicator"
-                        height={100}
-                        width={100}
-                        strokeWidth={4}
-                        color="#F28705"
-                        secondaryColor="none"
-                    />
-                </Loading>
-            </Container>
-        )
+    variables: {
+      slug: slug
     }
-   
-    return(
-        <Container>
-          <Helmet>
-            <meta property="og:title" content={data?.moveisDb.name} />
-            <meta property="og:image" content={ slugImgPlus !== undefined ? getImgPlus.data?.imgPlus.imgPlus.url : data?.moveisDb.img.url} />
-            <meta property="og:type" content="website" />
-          </Helmet>
-            <Cmodal>
-                <SideBar>
-                <Compartilhar>
-                    <a href={`https://wa.me/?text=${texto}`} target="_blank" data-action="share/whatsapp/share">
-                        <AiOutlineWhatsApp size={30} />
-                    </a>
-                    <CopyToClipboard text={`${window.location.href}
+  })
+
+  const getImgPlus = useQuery<GetImgPlusProps>(GET_MOVEIS_IMG_PLUS_BY_SLUG, {
+    variables: {
+      slugImgPlus: slugImgPlus
+    }
+  })
+
+  const textUrl = `${window.location.href}\n\n${data?.moveisDb.name}.\n\n De uma olhada nesse móvel feito pela GMPlanejado.`
+
+
+  const texto = window.encodeURIComponent(textUrl);
+
+  if (!data) {
+    return (
+      <Container>
+        <Loading>
+          <Oval
+            ariaLabel="loading-indicator"
+            height={100}
+            width={100}
+            strokeWidth={4}
+            color="#F28705"
+            secondaryColor="none"
+          />
+        </Loading>
+      </Container>
+    )
+  }
+
+  return (
+    <>
+      <Helmet>
+        <meta property="og:title" content={data?.moveisDb.name} />
+        <meta property="og:image" content={slugImgPlus !== undefined ? getImgPlus.data?.imgPlus.imgPlus.url : data?.moveisDb.img.url} />
+        <meta property="og:type" content="website" />
+      </Helmet>
+      <Container>
+        <Cmodal>
+          <SideBar>
+            <Compartilhar>
+              <a href={`https://wa.me/?text=${texto}`} target="_blank" data-action="share/whatsapp/share">
+                <AiOutlineWhatsApp size={30} />
+              </a>
+              <CopyToClipboard text={`${window.location.href}
 
 ${data?.moveisDb.name}.
 
 De uma olhada nesse móvel feito pela GMPlanejado.`}>
-                    <a >
-                        <BiLink size={30}/>
-                    </a>
-                    </CopyToClipboard>
-                </Compartilhar>
+                <a >
+                  <BiLink size={30} />
+                </a>
+              </CopyToClipboard>
+            </Compartilhar>
 
-                <Close to={'/'}>
-                    <AiOutlineClose size={23} />
-                </Close>
-                </SideBar>
-                <CImagemPlus>
-                
-                <ImgPlus active={slugImgPlus === undefined ? true : false} to={`/moveis/${data.moveisDb.slug}`} key={data.moveisDb.img.url}> <img src={data.moveisDb.img.url} /></ImgPlus>
+            <Close to={'/'}>
+              <AiOutlineClose size={23} />
+            </Close>
+          </SideBar>
+          <CImagemPlus>
 
-                {data.moveisDb.imgPlus.map((imgPlus) => 
-                (
-                <ImgPlus active={slugImgPlus === imgPlus.slugImgPlus ? true : false} to={`/moveis/${data.moveisDb.slug}/${imgPlus.slugImgPlus}`} key={imgPlus.slugImgPlus}> <img src={imgPlus.imgPlus.url} /></ImgPlus>
-                )
-                )}
-                </CImagemPlus>
+            <ImgPlus active={slugImgPlus === undefined ? true : false} to={`/moveis/${data.moveisDb.slug}`} key={data.moveisDb.img.url}> <img src={data.moveisDb.img.url} /></ImgPlus>
 
-                <CImagem src={ slugImgPlus !== undefined ? getImgPlus.data?.imgPlus.imgPlus.url : data?.moveisDb.img.url} />
+            {data.moveisDb.imgPlus.map((imgPlus) =>
+            (
+              <ImgPlus active={slugImgPlus === imgPlus.slugImgPlus ? true : false} to={`/moveis/${data.moveisDb.slug}/${imgPlus.slugImgPlus}`} key={imgPlus.slugImgPlus}> <img src={imgPlus.imgPlus.url} /></ImgPlus>
+            )
+            )}
+          </CImagemPlus>
+
+          <CImagem src={slugImgPlus !== undefined ? getImgPlus.data?.imgPlus.imgPlus.url : data?.moveisDb.img.url} />
 
 
-                <CTexto>
-                    <h1>{data?.moveisDb.name}</h1>
-                    <p>{data?.moveisDb.description}</p>
-                </CTexto>
-            </Cmodal>
-        </Container>
-    )
+          <CTexto>
+            <h1>{data?.moveisDb.name}</h1>
+            <p>{data?.moveisDb.description}</p>
+          </CTexto>
+        </Cmodal>
+      </Container>
+    </>
+  )
 }
 
 export default Modal
