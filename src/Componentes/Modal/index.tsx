ad3075825712ff9209322f7adacb96904ddd0d60
MkDesignWeb <99919855+MkDesignWeb@users.noreply.github.com>
2023-04-27 18:54:11 -0300

    ToClipboard fix

--- src/Componentes/Modal/index.tsx
+++ src/Componentes/Modal/index.tsx
@@ -117,11 +117,7 @@ function Modal() {
               <a href={`https://wa.me/?text=${texto}`} target="_blank" data-action="share/whatsapp/share">
                 <AiOutlineWhatsApp size={30} />
               </a>
-              <CopyToClipboard text={`${window.location.href}
-
-${data?.moveisDb.name}.
-
-De uma olhada nesse móvel feito pela GMPlanejado.`}>
+              <CopyToClipboard text={`${window.location.href}${data?.moveisDb.name}`}>
                 <a >
                   <BiLink size={30} />
                 </a>
